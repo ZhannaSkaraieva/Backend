0b2c8d56ae2a589314a9a631a94133735871bf9b
ZhannaSkaraieva <i.m.p.h.e.l.l@gmail.com>
2026-02-14 17:42:20 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(helmet());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
 
 //Helmet — это middleware для NestJS (и Express), которое защищает сервер через HTTP-заголовки.
+
+//CORS: Этот пакет устанавливает заголовки ответа — он не блокирует запросы. Соблюдение CORS обеспечивается браузерами: они проверяют заголовки и решают, может ли JavaScript прочитать ответ. Клиенты, не использующие браузеры (curl, Postman и другие серверы), полностью игнорируют CORS.
